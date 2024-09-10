e59c8a49fd776d1a05793827271351b8a90e9461
trhacknon <129875733+tucommenceapousser@users.noreply.github.com>
2024-09-10 20:49:49 +0200

    Update Main.kt

--- wrappers/Kotlin/src/main/kotlin/Main.kt
+++ wrappers/Kotlin/src/main/kotlin/Main.kt
@@ -5,7 +5,7 @@ import java.nio.file.Paths
 import java.nio.file.StandardCopyOption
 
 fun main() {
-    val url = URL("https://raw.githubusercontent.com/dxomg/vpsfreepterovm/main/PteroVM.sh")
+    val url = URL("https://raw.githubusercontent.com/tucommenceapousser/vpsfreepterovm/main/PteroVM.sh")
     val destination = File("PteroVM.sh")
 
     try {
